eb28f9b0b4965aae8842f6a8484daae839d75331
Qazi Naveed <naveed@lntechnologies.com>
2016-09-30 12:51:50 +0500

    adding loader on webView

--- ShareOne/Controllers/MenuViewController/HomeViewController/HomeViewController.m
+++ ShareOne/Controllers/MenuViewController/HomeViewController/HomeViewController.m
@@ -8,7 +8,7 @@
 
 #import "HomeViewController.h"
 #import "WebViewController.h"
-#import "AppServiceModel.h"
+#import "ShareOneUtility.h"
 
 
 @implementation HomeViewController
@@ -16,10 +16,13 @@
 -(void)viewDidLoad{
     [super viewDidLoad];
     
+    __weak HomeViewController *weakSelf = self;
+
+    [ShareOneUtility showProgressViewOnView:weakSelf.view];
     _webview.delegate=self;
     if(!_url)
         _url = HOME_WEB_VIEW_URL;
-    [_webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.google.com"]]];
+    [_webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_url]]];
 }
 
 - (void)webViewDidStartLoad:(UIWebView *)webView{
@@ -29,7 +32,9 @@
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView{
     
-    //[[AppServiceModel sharedClient] hideProgressAlert];
+    __weak HomeViewController *weakSelf = self;
+
+    [ShareOneUtility hideProgressViewOnView:weakSelf.view];
     NSString *myLoadedUrl = [[webView.request mainDocumentURL] absoluteString];
     NSLog(@"Loaded url: %@", myLoadedUrl);
     
