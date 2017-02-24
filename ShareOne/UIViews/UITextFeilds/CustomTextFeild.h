9b08f7a8f3a3e8f536279f7b69e59d30fcabe0e3
Qazi Naveed <naveed@lntechnologies.com>
2017-02-24 14:56:22 +0500

    Working on theme

---
+++ ShareOne/UIViews/UITextFeilds/CustomTextFeild.h
@@ -0,0 +1,13 @@
+//
+//  CustomTextFeild.h
+//  ShareOne
+//
+//  Created by Qazi Naveed on 2/24/17.
+//  Copyright © 2017 Ali Akbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTextFeild : UITextField
+
+@end
---
+++ ShareOne/UIViews/UITextFeilds/CustomTextFeild.m
@@ -0,0 +1,50 @@
+//
+//  CustomTextFeild.m
+//  ShareOne
+//
+//  Created by Qazi Naveed on 2/24/17.
+//  Copyright © 2017 Ali Akbar. All rights reserved.
+//
+
+#import "CustomTextFeild.h"
+#import "Configuration.h"
+#import "UIColor+HexColor.h"
+#import "ShareOneUtility.h"
+
+
+@implementation CustomTextFeild
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self initialize];
+    }
+    return self;
+}
+
+
+-(id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if(self){
+        [self initialize];
+    }
+    return self;
+}
+
+- (void)initialize{
+    
+    Configuration *config = [ShareOneUtility getConfigurationFile];
+    UIColor *color = [UIColor colorWithHexString:config.buttonColor];
+    
+    [self setTintColor:color];
+}
+
+@end
