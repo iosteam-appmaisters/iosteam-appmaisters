171bb97aa86c605d3a2695d68e060dde8e97547c
Qazi Naveed <ios.dev@appmaisters.com>
2017-06-16 14:46:56 +0500

    adding new files

---
+++ ShareOne/Controllers/MenuViewController/DepositTutorialController/DepositTutorialController.h
@@ -0,0 +1,18 @@
+//
+//  DepositTutorialController.h
+//  ShareOne
+//
+//  Created by Qazi Naveed on 5/25/17.
+//  Copyright © 2017 Ali Akbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DepositTutorialController : UIViewController
+
+
+@property (nonatomic,weak) IBOutlet UIImageView *imageView;
+@property (nonatomic,weak) IBOutlet UIView *parentView;
+
+-(IBAction)actionButtonClicked:(id)sender;
+@end
