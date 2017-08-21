1fa4b655f7f2af44f6632a4c9b23cec3752adfdb
Qazi Naveed <ios.dev@appmaisters.com>
2017-08-21 17:36:27 +0500

    new files added

--- ShareOne/Entity/LoaderEntity/LoaderServices.m
+++ ShareOne/Entity/LoaderEntity/LoaderServices.m
@@ -35,7 +35,7 @@
 
     
     //getSuffixDict
-    NSArray *reqArr = [NSArray arrayWithObjects:getDevicesDict, nil];
+    NSArray *reqArr = [NSArray arrayWithObjects:getDevicesDict,getSuffixDict, nil];
     
     
     [[AppServiceModel sharedClient] createBatchOfRequestsWithObject:reqArr requestCompletionBlock:^(NSObject *response, NSString *responseObj) {
